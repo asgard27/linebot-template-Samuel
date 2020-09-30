359e7caba2dc14b7848a112151f78b4e79f8838a
Oliver <46078394+May27Oliver@users.noreply.github.com>
2020-09-30 22:46:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def handle_message(event):
 
     text = event.message.text
     # print(text)
-    text = text.encode('utf-8')
+#     text = text.encode('utf-8')
     if text == "Hi":
       reply_text = "Hello, 你好嗎？"
     elif text == "你好":
@@ -45,14 +45,16 @@ def handle_message(event):
     else:
       reply_text = "Hola, 想問米機器人什麼呢？"
 
-    msg = event.message.text
-    #print(type(msg))
-    msg = msg.encode('utf-8')  
-    if event.message.text == "文字":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-    elif event.message.text == "貼圖":
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=3, sticker_id=183))
-    return 'OK2'
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+#     msg = event.message.text
+#     #print(type(msg))
+#     msg = msg.encode('utf-8')  
+#     if event.message.text == "文字":
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+#     elif event.message.text == "貼圖":
+#         line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=3, sticker_id=183))
+#     return 'OK2'
         
      
 if __name__ == "__main__":
